b1e0a83494c979ca848919a8e3a759d6fdd765a7
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2026-01-02 10:02:48 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Imports
+import a_GetFileContents as getFile
+
+# Notify the user that the program is ready to run
+print('Ready when you are\n')
+
+# Call 'a_GetFileContents' to begin the program
+getFile.getTheFile()
