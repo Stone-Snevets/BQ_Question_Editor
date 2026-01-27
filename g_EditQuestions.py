b59c059a2d730bbe9d00fa3185c2ae2f36d6618d
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2026-01-27 16:52:55 -0500

    Updated to Version 1.0.1

--- g_EditQuestions.py
+++ g_EditQuestions.py
@@ -7,6 +7,8 @@ def edit_questions__formality(df):
     - The question doesn't end with proper punctuation ('?' or '.')
     - The question asks for a "complete reference", but the chapter was given in the introductory remarks
     - The question asks for "references" without the word complete, but the chapter was not given in the introductory remarks
+    - "According to *verse*" questions come from the section
+      -> Unless the question is asking for the opening/closing verse
     - "According to *verse*" questions come from nowhere
       -> no chapter or section given in the introductory remarks
       -> Unless the chapter, section or book is given in the actual question or...
@@ -33,7 +35,7 @@ def edit_questions__formality(df):
         #-> If there is only one question that meets this criteria
         if len(df.loc[(df['Q_Num'] == 1) & (df['Pt_Val'] == 30)]) == 1:
             # If so, send the output for one question
-            list_flags__formality.append(f'<p>* Set {list(df.loc[((df['Q_Num'] == 1) & (df['Pt_Val'] == 30)), 'Set_Num'])[0]} question 1 is for 30 points. Consider moving this elsewhere in the set.</p>')
+            list_flags__formality.append(f'<p>* Set {list(df.loc[((df['Q_Num'] == 1) & (df['Pt_Val'] == 30)), 'Set_Num'])[0]: 03d} question 1 is for 30 points. Consider moving this elsewhere in the set.</p>')
         else: # If not, send the output for multiple questions
             list_flags__formality.append(f'<p>* Sets (<i>{', '.join(map(str,df.loc[((df['Q_Num'] == 1) & (df['Pt_Val'] == 30)), 'Set_Num']))}</i>): Question number 1 is for 30 points. Consider moving this elsewhere in the set.</p>')
 
@@ -46,7 +48,7 @@ def edit_questions__formality(df):
         if first_word == first_word.lower():
             # If so, grab the set number and question number of that question and...
             # Throw an output flag
-            list_flags__formality.append(f'<p>! Set {df['Set_Num'].iloc[i]} question {df['Q_Num'].iloc[i]} starts with a lowercase word. This should be capitalized.</p>')
+            list_flags__formality.append(f'<p>! Set {df['Set_Num'].iloc[i]: 03d} question {df['Q_Num'].iloc[i]: 03d} starts with a lowercase word. This should be capitalized.</p>')
     
     # === Check if the question doesn't end with "?" or "." ===
     # For each question
@@ -57,7 +59,7 @@ def edit_questions__formality(df):
         #-> The whitespace can appear at the end of quotation/essence completion questions
         if not re.search(r'[.?][ "]*\Z', df['Question'].iloc[i]):
             # If this is not the case either, throw a flag
-            list_flags__formality.append(f'<p>! Set {df['Set_Num'].iloc[i]} question {df['Q_Num'].iloc[i]} does not end with proper punctuation (<i>? .</i>). These should end the question.</p>')
+            list_flags__formality.append(f'<p>! Set {df['Set_Num'].iloc[i]: 03d} question {df['Q_Num'].iloc[i]: 03d} does not end with proper punctuation (<i>? .</i>). These should end the question.</p>')
 
     # === Check if the question asks for "complete reference", but the chapter was given in the introductory remarks ===
     # Check if there are any questions asking for "complete reference" and...
@@ -67,7 +69,7 @@ def edit_questions__formality(df):
         for i in range(len(df.loc[(df['Question'].str.contains('omplete reference')) & (df['Location'].str.contains('ch'))])):
             # Grab the set number and the question number, and...
             # Throw a flag
-            list_flags__formality.append(f'<p>* Set {df['Set_Num'].iloc[i]} quesiton {df['Q_Num'].iloc[i]} is asking for the "complete reference(s)" when the chapter is already given in the introductory remarks. This should just ask for the "references".</p>')
+            list_flags__formality.append(f'<p>* Set {df['Set_Num'].iloc[i]: 03d} quesiton {df['Q_Num'].iloc[i]: 03d} is asking for the "complete reference(s)" when the chapter is already given in the introductory remarks. This should just ask for the "references".</p>')
 
     # === Check if the question asks for only references, but the chapter is NOT in the introductory remarks ===
     # Check if there are any questions asking for "reference" that are NOT asking for "complete reference" and...
@@ -77,8 +79,24 @@ def edit_questions__formality(df):
         for i in range(len(df.loc[(df['Question'].str.contains('reference')) & (df['Question'].str.contains('omplete') == False) & (df['Location'] == '_')])):
             # Grab the set number and the question number, and...
             # Throw a flag
-            list_flags__formality.append(f'<p>* Set {df['Set_Num'].iloc[i]} quesiton {df['Q_Num'].iloc[i]} is asking for just the "reference(s)" when the chapter is not given. Either the chapter needs to be in the introductory remarks or the question needs to ask for "complete reference(s)".</p>')
+            list_flags__formality.append(f'<p>* Set {df['Set_Num'].iloc[i]: 03d} question {df['Q_Num'].iloc[i]: 03d} is asking for just the "reference(s)" when the chapter is not given. Either the chapter needs to be in the introductory remarks or the question needs to ask for "complete reference(s)".</p>')
 
+    # === Check if "According to *verse*" questions are coming from a section
+    # Check if there are any questions with the "According to *verse*" note and...
+    # Check if any of those questions are coming from a section and...
+    # Check if these questions aren't asking from the opening/closing verse
+    if len(df.loc[(df['Notes'] == 'According to *verse*') & (df['Location'].str.contains('sec')) & (df['Question'].str.contains(r'opening|closing') == False)]) > 0:
+        # If so, create a sub dataframe of all these instances
+        df_acc_sec = df.loc[
+                            (df['Notes'] == 'According to *verse*') &
+                            (df['Location'].str.contains('sec')) &
+                            (df['Question'].str.contains(r'opening|closing') == False)
+                           ]
+        # For each instance
+        for i in range(len(df_acc_sec)):
+            # Send a flag to the output
+            list_flags__formality.append(f'<p>! Set {df_acc_sec['Set_Num'].iloc[i]: 03d} question {df_acc_sec['Q_Num'].iloc[i]: 03d} asks an "According to *verse*" question but gives a section in the introductory remarks. This should be a chapter instead.</p>')
+    
     # === Check if "According to *verse*" questions are coming from nowhere
     # Check if there are any questions with the "According to *verse*" note and...
     # Check if any of those questions are coming from nowhere
@@ -91,7 +109,7 @@ def edit_questions__formality(df):
             # Check if the question is asking for the opening/closing verse
             if 'chapter' not in df_acc['Question'].iloc[i] and 'section' not in df_acc['Question'].iloc[i] and 'opening' not in df_acc['Question'].iloc[i] and 'closing' not in df_acc['Question'].iloc[i]:
                 # If neither of these conditions are met, throw a flag
-                list_flags__formality.append(f'<p>! Set {df_acc['Set_Num'].iloc[i]} question {df_acc['Q_Num'].iloc[i]} gives no chapter for "According to *verse*" question. This should be added.</p>')
+                list_flags__formality.append(f'<p>! Set {df_acc['Set_Num'].iloc[i]: 03d} question {df_acc['Q_Num'].iloc[i]: 03d} gives no chapter for "According to *verse*" question. This should be added.</p>')
 
     # === Check if the number of part questions is the same as the number of part answers ===
     # Create a sub dataframe of all questions that are both multiple-part question and multiple-part answer
@@ -101,7 +119,7 @@ def edit_questions__formality(df):
         # Determine if the number of questions is the same as the number of answers
         if re.findall(r'\d+', df_mpq_mpa['Q_Intro'].iloc[i]) == re.findall(r'\d+', df_mpq_mpa['A_Intro'].iloc[i]):
             # If so, throw a flag
-            list_flags__formality.append(f'<p>! Set {df_mpq_mpa['Set_Num'].iloc[i]} question {df_mpq_mpa['Q_Num'].iloc[i]} has the same number of part questions as part answers (<i>{re.findall(r'\d+', df_mpq_mpa['Q_Intro'].iloc[i])[0]}</i>). The number of part answers needs to go away or be changed.</p>')
+            list_flags__formality.append(f'<p>! Set {df_mpq_mpa['Set_Num'].iloc[i]: 03d} question {df_mpq_mpa['Q_Num'].iloc[i]: 03d} has the same number of part questions as part answers (<i>{re.findall(r'\d+', df_mpq_mpa['Q_Intro'].iloc[i])[0]}</i>). The number of part answers needs to go away or be changed.</p>')
 
     # Return the list of flags
     return list_flags__formality
@@ -139,7 +157,7 @@ def edit_questions__sep_consec(df):
         # For each question in the sub dataframe
         for i in range(len(df_sep_consec_10)):
             # Send a flag to the output
-            list_flags__sep_consec.append(f'<p>* Set {df_sep_consec_10['Set_Num'].iloc[i]} question {df_sep_consec_10['Q_Num'].iloc[i]} is a 10-point question coming from multiple verses. This is generally frowned upon even if the answers are short. Consider making this a 20/30-point question.</p>')
+            list_flags__sep_consec.append(f'<p>* Set {df_sep_consec_10['Set_Num'].iloc[i]: 03d} question {df_sep_consec_10['Q_Num'].iloc[i]: 03d} is a 10-point question coming from multiple verses. This is generally frowned upon even if the answers are short. Consider making this a 20/30-point question.</p>')
     
     # === Check if there are multiple verses in the answer, but the question doesn't indicate so ===
     # === Check if the question is coming from multiple verses, but the introductory marks don't mark it so ===
@@ -148,10 +166,19 @@ def edit_questions__sep_consec(df):
     # For each question in this sub dataframe
     for i in range(len(df_multi_ref)):
         # Determine if the question is not appropriately marked as separate/consecutive verses, and...
-        # Determine if the question is not asking for the (complete) references
-        if ('S' not in df_multi_ref['Location'].iloc[i]) and ('C' not in df_multi_ref['Location'].iloc[i]) and ('chs' not in df_multi_ref['Location'].iloc[i]) and ('secs' not in df_multi_ref['Location'].iloc[i]) and ('bks' not in df_multi_ref['Location'].iloc[i]) and ('reference' not in df_multi_ref['Question'].iloc[i]):
+        # Determine if the question is not asking for the (complete) references, and...
+        # Determine if the question is not a Quotation/Essence Completion question
+        if (
+            ('S' not in df_multi_ref['Location'].iloc[i]) and 
+            ('C' not in df_multi_ref['Location'].iloc[i]) and 
+            ('chs' not in df_multi_ref['Location'].iloc[i]) and 
+            ('secs' not in df_multi_ref['Location'].iloc[i]) and 
+            ('bks' not in df_multi_ref['Location'].iloc[i]) and 
+            ('reference' not in df_multi_ref['Question'].iloc[i]) and
+            ('C' not in df_multi_ref['Q_Intro'].iloc[i])
+           ):
             # If both conditions are met, throw a flag
-            list_flags__sep_consec.append(f'<p>! Set {df_multi_ref['Set_Num'].iloc[i]} question {df_multi_ref['Q_Num'].iloc[i]} has multiple references in the answer, but is not labeled as separate/consecutive verses. The label should be added.</p>')
+            list_flags__sep_consec.append(f'<p>! Set {df_multi_ref['Set_Num'].iloc[i]: 03d} question {df_multi_ref['Q_Num'].iloc[i]: 03d} has multiple references in the answer, but is not labeled as separate/consecutive verses. The label should be added.</p>')
 
     # === Check if the question is marked as separate/consecutve verses, but the answer is coming from one verse ===
     # Create a sub dataframe containing all questions marked as separate/consecutive verses
@@ -161,7 +188,7 @@ def edit_questions__sep_consec(df):
         # Check if there are multiple references in the answer
         if len(re.findall(':', df_sep_consec['Ans_Reference'].iloc[i])) < 2:
             # If not, throw a flag
-            list_flags__sep_consec.append(f'<p>! Set {df_sep_consec['Set_Num'].iloc[i]} question {df_sep_consec['Q_Num'].iloc[i]} is labeled as separate/consecutive verses, but only one verse is in the answer. Either add verses or remove this introductory remark.</p>')
+            list_flags__sep_consec.append(f'<p>! Set {df_sep_consec['Set_Num'].iloc[i]: 03d} question {df_sep_consec['Q_Num'].iloc[i]: 03d} is labeled as separate/consecutive verses, but only one verse is in the answer. Either add verses or remove this introductory remark.</p>')
 
     # === Check if a standard quotation/essence question is from multiple verses, but isn't marked as such ===
     # === Check if a standard quotation/essence question from multiple verses doesn't have the number of verses marked ===
@@ -174,15 +201,15 @@ def edit_questions__sep_consec(df):
         # Determine if the questions are labeled as separate/consecutive verses
         if ('S' not in df_std['Location'].iloc[i]) and ('C' not in df_std['Location'].iloc[i]):
             # If not, throw a flag
-            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]} question {df_std['Q_Num'].iloc[i]} MUST be labeled as separate/consecutive verses.</p>')
+            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]: 03d} question {df_std['Q_Num'].iloc[i]: 03d} MUST be labeled as separate/consecutive verses.</p>')
         # Determine if the questions have the number of verses in the introductory remarks
         if re.search(r'\d+', df_std['Location'].iloc[i]) == None:
             # If not, throw a flag
-            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]} question {df_std['Q_Num'].iloc[i]} MUST have the number of verses in the introductory remarks.</p>')
+            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]: 03d} question {df_std['Q_Num'].iloc[i]: 03d} MUST have the number of verses in the introductory remarks.</p>')
         # Determine if the questions are labeled as a multiple-part question
         if re.search(r'\d+', df_std['Q_Intro'].iloc[i]) != None:
             # If so, throw a flag
-            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]} question {df_std['Q_Num'].iloc[i]} must NOT be labeled as a multiple-part question.</p>')
+            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]: 03d} question {df_std['Q_Num'].iloc[i]: 03d} must NOT be labeled as a multiple-part question.</p>')
 
 
     # === Check if a question asking for (complete) references is marked as separate/consecutive verses ===
@@ -193,7 +220,7 @@ def edit_questions__sep_consec(df):
         # Determine if the questions are labeled as separate/consecutive verses
         if 'S' in df_complete_refs['Location'].iloc[i] or 'C' in df_complete_refs['Location'].iloc[i]:
             # If so, throw a flag
-            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]} question {df_std['Q_Num'].iloc[i]} asks for (complete) references, but it is labeled as separate/consecutive verses. The label needs to be removed.</p>')
+            list_flags__sep_consec.append(f'<p>! Set {df_std['Set_Num'].iloc[i]: 03d} question {df_std['Q_Num'].iloc[i]: 03d} asks for (complete) references, but it is labeled as separate/consecutive verses. The label needs to be removed.</p>')
 
     # Return the list of flags
     return list_flags__sep_consec
@@ -230,13 +257,13 @@ def edit_questions__scripture_text(df):
         # Check if that question contains the phrase "end quote"
         if ('end quote' not in df_quote['Question'].iloc[i]) and ('end-quote' not in df_quote['Question'].iloc[i]):
             # If not, throw a flag
-            list_flags__scripture_text.append(f'<p>! Set {df_quote['Set_Num'].iloc[i]} question {df_quote['Q_Num'].iloc[i]} may need to be labeled as a Scripture-Text question.</p>')
+            list_flags__scripture_text.append(f'<p>! Set {df_quote['Set_Num'].iloc[i]: 03d} question {df_quote['Q_Num'].iloc[i]: 03d} may need to be labeled as a Scripture-Text question.</p>')
 
     # === Check for questions labeled as Scripture-Text that don't have the word "quote" ===
     # For each question labeled Scripture-Text without the word "quote,"
     for i in range(len(df.loc[(df['Q_Intro'].str.contains('T')) & (df['Question'].str.contains('quote,') == False)])):
         # Throw a flag
-        list_flags__scripture_text.append(f'<p>! Set {df.loc[(df['Q_Intro'].str.contains('T')) & (df['Question'].str.contains('quote,') == False)]['Set_Num'].iloc[i]} question {df.loc[(df['Q_Intro'].str.contains('T')) & (df['Question'].str.contains('quote,') == False)]['Q_Num'].iloc[i]} is labeled as Scripture-Text but does not have a scripture text at the end of the question (denoted by "quote"). Consider adding this or removing the Scripture-Text introductory remark.</p>')
+        list_flags__scripture_text.append(f'<p>! Set {df.loc[(df['Q_Intro'].str.contains('T')) & (df['Question'].str.contains('quote,') == False)]['Set_Num'].iloc[i]: 03d} question {df.loc[(df['Q_Intro'].str.contains('T')) & (df['Question'].str.contains('quote,') == False)]['Q_Num'].iloc[i]: 03d} is labeled as Scripture-Text but does not have a scripture text at the end of the question (denoted by "quote"). Consider adding this or removing the Scripture-Text introductory remark.</p>')
 
     # === Check for questions labeled Scripture-Text that have a verse reference in the question ===
 
@@ -273,7 +300,7 @@ def edit_questions__statement(df):
         # Check if there are either 2 periods or 1 period with 1 question mark
         if not re.search(r'[.][^.]+[.?]', df_statement['Question'].iloc[i]):
             # If not, throw a flag
-            list_flags__statement.append(f'<p>! Set {df_statement['Set_Num'].iloc[i]} question {df_statement['Q_Num'].iloc[i]} is labeled as a Statement and Question, but no statement is present. Either add a statement or remove the introductory remark.</p>')
+            list_flags__statement.append(f'<p>! Set {df_statement['Set_Num'].iloc[i]: 03d} question {df_statement['Q_Num'].iloc[i]: 03d} is labeled as a Statement and Question, but no statement is present. Either add a statement or remove the introductory remark.</p>')
 
     # === Check if a question is NOT labeled as Statement and Question but has a statement ===
     # Create a sub dataframe of all questions with multiple punctuation (? of .) and...
@@ -284,7 +311,7 @@ def edit_questions__statement(df):
         # Check if the question is labeled as Statement and Question
         if re.search('S', df_multi_punct['Q_Intro'].iloc[i]) == None:
             # If not, throw a flag
-            list_flags__statement.append(f'<p>! Set {df_multi_punct['Set_Num'].iloc[i]} question {df_multi_punct['Q_Num'].iloc[i]} appears to have a statement in the question, but the question is not labeled Statement and Question.  Consider adding this introductory remark.</p>')
+            list_flags__statement.append(f'<p>! Set {df_multi_punct['Set_Num'].iloc[i]: 03d} question {df_multi_punct['Q_Num'].iloc[i]: 03d} appears to have a statement in the question, but the question is not labeled Statement and Question.  Consider adding this introductory remark.</p>')
 
     # Return the list of flags
     return list_flags__statement
@@ -300,6 +327,8 @@ def edit_questions(df):
     - The question doesn't end with proper punctuation ('?' or '.')
     - The question asks for a "complete reference", but the chapter was given in the introductory remarks
     - The question asks for "references" without the word complete, but the chapter was not given in the introductory remarks
+    - "According to *verse*" questions come from a section
+      -> Unless it is the opening/closing verse
     - "According to *verse*" questions come from nowhere
       -> no chapter, section, or book given in the introductory remarks
       -> Unless the chapter or section is given in the actual question or...
